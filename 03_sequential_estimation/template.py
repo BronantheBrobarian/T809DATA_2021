ebb0586e5bf83d5145bb1d03fc7ec944f6e3cddd
baldur.kr <baldur.kr@users.noreply.github.com>
2021-12-03 19:38:44 +0000

    something

--- 03_sequential_estimation/template.py
+++ 03_sequential_estimation/template.py
@@ -1,7 +1,9 @@
+from numpy.random.mtrand import random
 from tools import scatter_3d_data, bar_per_axis
 
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 
 def gen_data(
@@ -13,8 +15,45 @@ def gen_data(
     '''Generate n values samples from the k-variate
     normal distribution
     '''
-    ...
+    
+    X = [n, k]
+    for i in range(n):
+        for j in range(k):
+            X[i, j] = np.random.multivariate_normal(mean, var, k)
 
+    return X
+
+def gen(
+    n: int,
+    k: int,
+    mean: np.ndarray,
+    var: float
+) -> np.ndarray:
+    '''Generate n values samples from the k-variate
+    normal distribution
+    '''
+    T = [n, k]
+    for i in range(k):
+        T[n, i] = randint(-1, 1)
+    print(T)
+
+
+    X = [n, k]
+    for i in range(k):
+        X[n, i] = np.random.multivariate_normal(mean, X, var)
+    
+    return X
+
+
+#mean = [0, 0]
+#cov = [[1, 0], [0, 100]]  # diagonal covariance
+#x, y = np.random.multivariate_normal(mean, cov, 5000).T
+#plt.plot(x, y, 'x')
+#plt.axis('equal')
+#plt.show()
+
+array = gen(2, 3, np.array([0, 1, -1]), 1.3)
+print(f'array: {array}')
 
 def update_sequence_mean(
     mu: np.ndarray,
